9a2d3d5aa7ffff9c75e5e411aa308ca57d17785a
BUMJH0121 <bumjh0121@gmail.com>
2021-02-01 15:48:44 +0900

    add welcome

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello node.js'));
+app.get('/welcome', (req, res) => res.send('welcome_page'));
 app.listen(port, () => console.log('Example app listening on port 3000'))
