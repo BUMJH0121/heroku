bd487f7439700460e3ef2bc98fba55ac8279d7d3
BUMJH0121 <bumjh0121@gmail.com>
2021-02-01 16:39:40 +0900

    second

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello node.js'));
-app.get('/welcome', (req, res) => res.send('welcome_page'));
+app.get('/welcome', (req, res) => {
+    var user_name = req.param('name');
+    res.send('welcome ['+ user_name +']');
+});
 app.get('/check', (req, res) => res.send('check_page'));
 app.listen(port, () => console.log('Example app listening on port 3000'))
